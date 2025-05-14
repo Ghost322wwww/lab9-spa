ab5614ff808b25e105e6de93b4e2fb9a4b2ecc4b
Ghost322wwww <kwokc289@yahoo.com>
2025-05-14 21:02:41 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
-import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import App from './App'
+import DetailArticle from './components/DetailArticle'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/a/:id" element={<DetailArticle />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 )
