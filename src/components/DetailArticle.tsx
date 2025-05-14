fc00642357e537400ff366b2deafc33ea5942ce1
Ghost322wwww <kwokc289@yahoo.com>
2025-05-14 21:02:38 +0800

    Create DetailArticle.tsx

---
+++ src/components/DetailArticle.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { useParams } from 'react-router-dom'
+import articles from './data/articles.json'
+
+const DetailArticle = () => {
+  const { id } = useParams()
+  const article = articles.find((a) => a.id === Number(id))
+
+  if (!article) {
+    return <h1>Article does not exist</h1>
+  }
+
+  return (
+    <div>
+      <h1>{article.title}</h1>
+      <p>{article.fullText}</p>
+    </div>
+  )
+}
+
+export default DetailArticle
