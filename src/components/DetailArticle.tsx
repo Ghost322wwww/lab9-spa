c735eba915b5cf7541e8b3c5a0b52501619b7bb7
Ghost322wwww <kwokc289@yahoo.com>
2025-05-15 18:43:39 +0800

    Update DetailArticle.tsx

--- src/components/DetailArticle.tsx
+++ src/components/DetailArticle.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import { useParams, Link } from 'react-router-dom'
-import articles from './data/articles.json'
+import React from 'react';
+import { useParams, Link } from 'react-router-dom';
+import articles from './data/articles.json';
 
 const DetailArticle = () => {
-  const { id } = useParams()
-  const article = articles.find((a) => a.id === Number(id))
+  const { aid } = useParams();
+  const article = articles.find((a) => a.id === Number(aid));
 
   if (!article) {
-    return <h1>This article cannot be found</h1>
+    return <h1>This article cannot be found</h1>;
   }
 
   return (
-    <div>
+    <div style={{ padding: 20 }}>
       <h1>{article.title}</h1>
       <p>{article.fullText}</p>
       <br />
       <Link to="/">Back to Home Page</Link>
     </div>
-  )
-}
+  );
+};
 
-export default DetailArticle
+export default DetailArticle;
