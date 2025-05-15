33e5ecaa8a0d7a6e24afa010d22cb27f20a050c2
Ghost322wwww <kwokc289@yahoo.com>
2025-05-15 18:43:37 +0800

    Create Login.tsx

---
+++ src/components/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Login Page</h1>
+      <p>This is a placeholder for the login form.</p>
+    </div>
+  )
+}
+
+export default Login
