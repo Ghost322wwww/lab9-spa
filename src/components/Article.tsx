559f31755b3404572c4c3e1d6d32347a47e93298
Ghost322wwww <kwokc289@yahoo.com>
2025-05-15 19:15:36 +0800

    Update Article.tsx

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -3,19 +3,42 @@ import { Link } from 'react-router-dom'
 import { Card, Row, Col } from 'antd'
 import articles from './data/articles.json'
 
-
 const Article = () => {
   return (
-    <Row justify="space-around" gutter={[16, 16]}>
-      {articles.map(({ id, title, fullText }) => (
-        <Col span={6} key={id}>
-          <Card title={title} style={{ width: 300 }}>
-            <p>{fullText}</p>
-            <Link to={`/a/${id}`}>Details</Link>
-          </Card>
-        </Col>
-      ))}
-    </Row>
+    <div style={{ padding: '40px 80px' }}>
+  <Row gutter={[24, 32]} justify="start">
+    {articles.map(({ id, title, fullText, img }) => (
+      <Col xs={24} sm={12} md={8} lg={6} key={id}>
+        <Card
+          hoverable
+          style={{ width: '350', borderRadius: 10 }}
+          cover={
+            <img
+              alt={title}
+              src={img}
+              style={{
+                height: 22,
+                objectFit: 'cover',
+                borderRadius: '10px 10px 0 0',
+              }}
+            />
+          }
+        >
+          <Card.Meta
+            title={<div style={{ fontSize: '1.1rem' }}>{title}</div>}
+            description={
+              <div style={{ marginTop: 10 }}>
+                <p style={{ fontSize: '0.95rem' }}>{fullText.slice(0, 60)}...</p>
+                <Link to={`/a/${id}`}>Details</Link>
+              </div>
+            }
+          />
+        </Card>
+      </Col>
+    ))}
+  </Row>
+
+    </div>
   )
 }
 
