4008f362e467349b33f124e83c22a4c4235cf67e
Ghost322wwww <kwokc289@yahoo.com>
2025-05-15 18:43:34 +0800

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 
 import App from './App'
-import DetailArticle from './components/DetailArticle'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/a/:id" element={<DetailArticle />} />
-      </Routes>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 )
