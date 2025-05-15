6c4391820f6bcd896bca12027e0a40c7c432e1a9
Ghost322wwww <kwokc289@yahoo.com>
2025-05-15 18:43:35 +0800

    Create Register.tsx

---
+++ src/components/Register.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Register Page</h1>
+      <p>This is a placeholder for the registration form.</p>
+    </div>
+  )
+}
+
+export default Register
