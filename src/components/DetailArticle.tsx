12d967374dff5bdaa500f8e56558fa27082a0f9a
Ghost322wwww <kwokc289@yahoo.com>
2025-05-15 16:44:49 +0800

    Update DetailArticle.tsx

--- src/components/DetailArticle.tsx
+++ src/components/DetailArticle.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, Link } from 'react-router-dom'
 import articles from './data/articles.json'
 
 const DetailArticle = () => {
@@ -7,13 +7,15 @@ const DetailArticle = () => {
   const article = articles.find((a) => a.id === Number(id))
 
   if (!article) {
-    return <h1>Article does not exist</h1>
+    return <h1>This article cannot be found</h1>
   }
 
   return (
     <div>
       <h1>{article.title}</h1>
       <p>{article.fullText}</p>
+      <br />
+      <Link to="/">Back to Home Page</Link>
     </div>
   )
 }
