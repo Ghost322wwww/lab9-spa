737e8374aa60881d3d5d9831692ac986a2492e93
Ghost322wwww <kwokc289@yahoo.com>
2025-05-15 19:21:00 +0800

    Update Article.tsx

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -5,39 +5,38 @@ import articles from './data/articles.json'
 
 const Article = () => {
   return (
-    <div style={{ padding: '40px 80px' }}>
-  <Row gutter={[24, 32]} justify="start">
-    {articles.map(({ id, title, fullText, img }) => (
-      <Col xs={24} sm={12} md={8} lg={6} key={id}>
-        <Card
-          hoverable
-          style={{ width: '350', borderRadius: 10 }}
-          cover={
-            <img
-              alt={title}
-              src={img}
-              style={{
-                height: 22,
-                objectFit: 'cover',
-                borderRadius: '10px 10px 0 0',
-              }}
-            />
-          }
-        >
-          <Card.Meta
-            title={<div style={{ fontSize: '1.1rem' }}>{title}</div>}
-            description={
-              <div style={{ marginTop: 10 }}>
-                <p style={{ fontSize: '0.95rem' }}>{fullText.slice(0, 60)}...</p>
-                <Link to={`/a/${id}`}>Details</Link>
-              </div>
-            }
-          />
-        </Card>
-      </Col>
-    ))}
-  </Row>
-
+    <div style={{ padding: '40px 60px' }}>
+      <Row gutter={[200, 40]} justify="start">
+        {articles.map(({ id, title, fullText, img }) => (
+          <Col key={id} xs={24} sm={12} md={12} lg={8} xl={6}>
+            <Card
+              hoverable
+              style={{ width: 400, borderRadius: 10 }}
+              cover={
+                <img
+                  alt={title}
+                  src={img}
+                  style={{
+                    height: 250,
+                    objectFit: 'cover',
+                    borderRadius: '10px 10px 0 0',
+                  }}
+                />
+              }
+            >
+              <Card.Meta
+                title={<div style={{ fontSize: '1.2rem', fontWeight: 'bold' }}>{title}</div>}
+                description={
+                  <div style={{ marginTop: 10 }}>
+                    <p style={{ fontSize: '1rem' }}>{fullText.slice(0, 60)}...</p>
+                    <Link to={`/a/${id}`}>Details</Link>
+                  </div>
+                }
+              />
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </div>
   )
 }
